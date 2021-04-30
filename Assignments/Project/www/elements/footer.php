b27f0cdbde6fb78b7f3b48e8fda272af7d8a5aaa
RAGHAD ALSHARAFI <58725913+raghadMSF@users.noreply.github.com>
2021-04-30 03:12:17 +0300

    Create footer.php

---
+++ Assignments/Project/www/elements/footer.php
@@ -0,0 +1,11 @@
+<?php
+echo '
+</div>
+<footer class="py-3">
+<p class="text-center">Gym Booking Website</p>
+<p class="text-center text-muted">Raghad Alsharafi © 2021</p>
+</footer>
+<script src="/gym-project/src/javascript/script.js"></script>
+</body>
+</html>
+';
